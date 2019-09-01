92b77d59202be8042536bd217f16a7aca257ae73
Prateek Malhotra <malhotraprateek26@gmail.com>
2019-09-01 14:59:24 +0530

    Created a static library project to ease tinygltf includion into projects

---
+++ itinygltf.h
@@ -0,0 +1,8 @@
+
+#undef TINYGLTF_IMPLEMENTATION
+#undef STB_IMAGE_IMPLEMENTATION
+#undef STB_IMAGE_WRITE_IMPLEMENTATION
+#undef STBI_MSC_SECURE_CRT
+
+#include "tiny_gltf.h"
+#include "stb_image.h"
